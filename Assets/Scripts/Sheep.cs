437dcaba228786659906f5df1e482325ad44dbe6
Unknown <tommaso.loiacono01@gmail.com>
2018-01-27 11:53:16 +0100

    Commit

---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+    }
+}
---
+++ Assets/Scripts/SheepManagement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepManagement : MonoBehaviour
+{
+    public GameObject sheep;
+    public float minNumberOfSheeps;
+    public float maxNumberOfSheeps;
+    public float yRandomDisplacement, xRandomDisplacement;
+
+    private float layerWidth, layerHeight;
+    private int numberOfSheeps;
+
+    private void Start()
+    {
+        Populate();
+    }
+
+    internal void Populate()
+    {
+        layerWidth = GetComponent<BoxCollider2D>().size.x/2;
+        layerHeight = GetComponent<BoxCollider2D>().size.y/2;
+
+        numberOfSheeps = (int)UnityEngine.Random.Range(minNumberOfSheeps, maxNumberOfSheeps);
+
+        for (int i = 0; i < numberOfSheeps; i++)
+        {
+            var spawnPos = new Vector3(randomDisplacement(layerWidth),
+                randomDisplacement(layerHeight),
+                0f);
+            Instantiate(sheep, spawnPos, Quaternion.identity, transform);
+        }
+    }
+
+    float randomDisplacement(float width)
+    {
+        return UnityEngine.Random.Range(-1f, 1f) * width;
+    }
+}
