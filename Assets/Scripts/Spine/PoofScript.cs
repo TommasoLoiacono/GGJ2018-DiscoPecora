50b840e12454b0664cabe1ffae3c80d67ddb5f27
John Laptop <jbaxter1337@yahoo.com>
2018-01-28 04:46:46 -0800

    Added poof with delegate disapear.. Instantiate poof at target location and set its pooftarget imediatly

---
+++ Assets/Scripts/Spine/PoofScript.cs
@@ -0,0 +1,39 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoofScript : MonoBehaviour
+{
+
+    // The [SpineEvent] attribute makes the inspector for this MonoBehaviour
+    // draw the field as a dropdown list of existing event names in your SkeletonData.
+    [SpineEvent] public string poofEvent = "pouf";
+
+    public GameObject poofTarget;
+
+    void Start()
+    {
+        var anim = GetComponent<SkeletonAnimation>();
+        if (anim == null) return;   // told you to add this to SkeletonAnimation's GameObject.
+
+        // This is how you subscribe via a declared method. The method needs the correct signature.
+        anim.state.Event += HandleEvent;
+
+        anim.state.SetAnimation(0, "animation", false);
+
+        anim.state.End += delegate {
+            Destroy(gameObject);
+        };
+
+    }
+
+    void HandleEvent(Spine.TrackEntry entry, Spine.Event e)
+    {
+
+        if (e.Data.Name == poofEvent)
+        {
+            GameObject.Destroy(poofTarget);
+        }
+    }
+}
